bba54364da8c209fd0eb8ceb8fc2b148b2da54f3
Anıl Şenay <anilsenay3@gmail.com>
2020-04-25 14:22:47 +0300

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,9 +1,15 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList;
 
 class MergeSort {
+
+	static ArrayList<Integer> list = new ArrayList<Integer>();
 	// Merges two subarrays of arr[].
 	// First subarray is arr[l..m]
 	// Second subarray is arr[m+1..r]
-	void merge(int arr[], int l, int m, int r) {
+	void merge(ArrayList<Integer> list, int l, int m, int r) {
 		// Find sizes of two subarrays to be merged
 		int n1 = m - l + 1;
 		int n2 = r - m;
@@ -14,9 +20,9 @@ class MergeSort {
 
 		/* Copy data to temp arrays */
 		for (int i = 0; i < n1; ++i)
-			L[i] = arr[l + i];
+			L[i] = list.get(l + i);
 		for (int j = 0; j < n2; ++j)
-			R[j] = arr[m + 1 + j];
+			R[j] = list.get(m + 1 + j);
 
 		/* Merge the temp arrays */
 
@@ -27,10 +33,10 @@ class MergeSort {
 		int k = l;
 		while (i < n1 && j < n2) {
 			if (L[i] <= R[j]) {
-				arr[k] = L[i];
+				list.set(k, L[i]);
 				i++;
 			} else {
-				arr[k] = R[j];
+				list.set(k, R[j]);
 				j++;
 			}
 			k++;
@@ -38,14 +44,14 @@ class MergeSort {
 
 		/* Copy remaining elements of L[] if any */
 		while (i < n1) {
-			arr[k] = L[i];
+			list.set(k, L[i]);
 			i++;
 			k++;
 		}
 
 		/* Copy remaining elements of R[] if any */
 		while (j < n2) {
-			arr[k] = R[j];
+			list.set(k, R[j]);
 			j++;
 			k++;
 		}
@@ -53,17 +59,17 @@ class MergeSort {
 
 	// Main function that sorts arr[l..r] using
 	// merge()
-	void sort(int arr[], int l, int r) {
+	void sort(ArrayList<Integer> list, int l, int r) {
 		if (l < r) {
 			// Find the middle point
 			int m = (l + r) / 2;
 
 			// Sort first and second halves
-			sort(arr, l, m);
-			sort(arr, m + 1, r);
+			sort(list, l, m);
+			sort(list, m + 1, r);
 
 			// Merge the sorted halves
-			merge(arr, l, m, r);
+			merge(list, l, m, r);
 		}
 	}
 
@@ -75,20 +81,40 @@ class MergeSort {
 		System.out.println();
 	}
 
+	static void printList(ArrayList<Integer> list){
+		int n = list.size();
+		for (int i = 0; i < n; ++i)
+			System.out.print(list.get(i) + " ");
+		System.out.println();
+	}
+
+	static void readFromFile(String fileName) {
+		try {
+      File myObj = new File(fileName);
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNext()) {
+        int data = myReader.nextInt();
+				list.add(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+	}
+
 	// Driver method
 	public static void main(String args[]) {
-		int arr[] = { 12, 11, 13, 5, 6, 7 };
-
-		System.out.println("Given Array");
-		printArray(arr);
-
+		readFromFile("input.txt");
 		MergeSort ob = new MergeSort();
-		ob.sort(arr, 0, arr.length - 1);
+		ob.sort(list, 0, list.size() - 1);
 
-		System.out.println("\nMiddle element: " + arr[(int) Math.ceil(arr.length / 2)]);
-		
+		System.out.println("\nMiddle element: " + list.get((int) Math.ceil(list.size() / 2)));
+
 		System.out.println("\nSorted array");
-		printArray(arr);
+		printList(list);
+
+
 	}
 }
 /* This code is contributed by Rajat Mishra */
