e2e87622a3115a6189ed76ffd5638636d60a239c
Bilgehan Gecici <bilgehangecici@gmail.com>
2020-04-29 12:36:28 +0300

    Update MedianOfThree.java

--- MedianOfThree.java
+++ MedianOfThree.java
@@ -1,25 +1,66 @@
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Collections;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
-public class MedianOfThree {
+public class QucikSelectMedianOfThree {
 
-  static ArrayList<Integer> arrayList = new ArrayList<Integer>();
+	// partition function similar to quick sort
+	// Considers first element as pivot and adds
+	// elements with less value to the left and
+	// high value to the right and also changes
+	// the pivot position to its respective position
+	// in the final array.
+	static ArrayList<Integer> arrayList = new ArrayList<Integer>();
+	static int counter = 0;
 
-	static int numSwaps = 0;
-	static int numComps = 0;
+	// finds the kth position (of the sorted array)
+	// in a given unsorted array i.e this function
+	// can be used to find both kth largest and
+	// kth smallest element in the array.
+	// ASSUMPTION: all elements in arr[] are distinct
+	public static int kthSmallest(ArrayList<Integer> list, int pivot, int high, int k) {
+		// find the partition
+		counter++;
+		long median = medianOf3(0, arrayList.size() - 1);
+		int partition = partition(0, arrayList.size() - 1, median);
 
+		// int partition = partition(list, pivot, high);
 
-  static void readFromFile(String fileName) {
+		// if partition value is equal to the kth position,
+		// return value at k.
+		if (partition == k) {
+			return list.get(partition);
+		}
+
+		// if partition value is less than kth position,
+		// search right side of the array.
+		else if (partition < k)
+			return kthSmallest(list, partition + 1, high, k);
+
+		// if partition value is more than kth position,
+		// search left side of the array.
+		else
+			return kthSmallest(list, pivot, partition - 1, k);
+
+	}
+
+	static void printList(ArrayList<Integer> list) {
+		int n = list.size();
+		for (int i = 0; i < n; ++i)
+			System.out.print(list.get(i) + " ");
+		System.out.println();
+	}
+
+	static void readFromFile(String fileName) {
 		try {
 			File myObj = new File(fileName);
 			Scanner myReader = new Scanner(myObj);
+
 			while (myReader.hasNext()) {
 				int data = myReader.nextInt();
 				arrayList.add(data);
@@ -31,122 +72,74 @@ public class MedianOfThree {
 		}
 	}
 
-	public static int medianPivot(ArrayList<Integer> arrayList, int low, int high) {
-		/*
-		 * create subarray with low, high, and middle elements in the array sort the
-		 * subarray and use index 1 as the median of 3
-		 */
+	public static int partition(int left, int right, long pivot) {
+		int leftPtr = left; // right of first elem
+		int rightPtr = right - 1; // left of pivot
 
-		int first = arrayList.get(low);
-		int last = arrayList.get(arrayList.size() - 1);
-		int mid = (high) / 2;
+		while (true) {
+			// find bigger
+			while (arrayList.get(++leftPtr) < pivot)
+				;
+			// find smaller
+			while (arrayList.get(--rightPtr) > pivot)
 
-		System.out.println("\tMiddle of Arr at Index= " + mid + " : " + arrayList.get(mid));
-		int[] sortingArr = { arrayList.get(low), arrayList.get(mid), arrayList.get(high) };
-
-		Arrays.sort(sortingArr);
-
-		int middleValue = sortingArr[1];
-		System.out.println("\t"+Arrays.toString(sortingArr));
-		//printList(sortingArr);
-
-		// swap with the last to serve as pivot
-		int temp = arrayList.get(high);
-    arrayList.set(high, middleValue);
-		if (middleValue == arrayList.get(low)) {
-			arrayList.set(low, temp);
-		} else if (middleValue == arrayList.get(mid)) {
-			arrayList.set(mid, temp);
+				;
+			if (leftPtr >= rightPtr) // if pointers cross, partition done
+				break;
+			else
+				// not crossed, so
+				swap(leftPtr, rightPtr); // swap elements
 		}
-
-		// System.out.println("median: ");
-		printList(arrayList);
-		return partition(arrayList, low, high);
-
+		swap(leftPtr, right - 1); // restore pivot
+		return leftPtr; // return pivot location
 	}
 
-	// ----------------------------------------------------------------------
-	/*  method for medianOfThree */
-	public static void medianOfThree(ArrayList<Integer> arrayList, int low, int high) {
-		if (low >= high)
-			return;
+	public static long medianOf3(int left, int right) {
+		int center = (left + right) / 2;
+		// order left & center
+		if (arrayList.get(left) > arrayList.get(center))
+			swap(left, center);
+		// order left & right
+		if (arrayList.get(left) > arrayList.get(right))
+			swap(left, right);
+		// order center & right
+		if (arrayList.get(center) > arrayList.get(right))
+			swap(center, right);
 
-		if (low < high) {
-
-			int pi = medianPivot(arrayList, low, high);
-
-			QuickSort(arrayList, low, high);
-
-		}
+		swap(center, right - 1); // put pivot on right
+		return arrayList.get(right - 1); // return median value
 	}
 
-	// -----------------------------------------------------------------------
-	public static void QuickSort(ArrayList<Integer> arrayList, int low, int high) {
-
-		if (low < high) {
-			int pi = partition(arrayList, low, high);
-
-			// Recursively sort elements before
-			// partition and after partition
-			QuickSort(arrayList, low, pi - 1);
-			QuickSort(arrayList, pi + 1, high);
-		}
+	public static void swap(int dex1, int dex2) {
+		long temp = arrayList.get(dex1);
+		arrayList.set(dex1, arrayList.get(dex2));
+		arrayList.set(dex2, (int) temp);
 	}
 
-	// ----------------------------------------------------------------------
-	/* A utility function to print array of size n */
-  private static void printList(ArrayList<Integer> arrayList) {
-		int n = arrayList.size();
-		for (int i = 0; i < n; ++i)
-			System.out.print(arrayList.get(i) + " ");
-
-		System.out.println();
-	}
-
-	// -----------------------------------------------------------------------
-	public static int partition(ArrayList<Integer> arrayList, int low, int high) {
-		int pivot = arrayList.get(high);
-		int i = (low - 1); // index of smaller element
-
-		for (int j = low; j < high; j++) {
-			// If current element is smaller than or
-			// equal to pivot
-			if (arrayList.get(j) <= pivot) {
-				i++;
-
-				// swap arr[i] and arr[j]
-				int temp = arrayList.get(i);
-				arrayList.set(i, arrayList.get(j));
-				arrayList.set(j, temp);
-				numSwaps++;
-			}
-			numComps++;
-		}
-
-		// swap arr[i+1] and arr[high] (or pivot)
-		int temp = arrayList.get(i+1);
-    arrayList.set(i+1, arrayList.get(high));
-    arrayList.set(high, temp);
-		numSwaps++;
-		return i + 1;
-
-	}
+	// Driver Code
 	public static void main(String[] args) {
 
-	    readFromFile("input.txt");
+		readFromFile("Cases/bestCase1.txt");
 
-			System.out.println("\n\n unsorted array:");
-			printList(arrayList);
-			System.out.println();
-			long startTime = System.nanoTime();
-			medianOfThree(arrayList, 0, arrayList.size()-1);
-			long endTime   = System.nanoTime();
-			long totalTime = endTime - startTime; 
-			System.out.println("\n\n sorted array:");
-			printList(arrayList);
+		int length = arrayList.size();
 
-			System.out.println("\n\tSwaps: " + numSwaps);
-			System.out.println("\tComparisons: " + numComps);
-			System.out.println("Total time in nanoseconds: " + totalTime);
+		int kPosition = (int) Math.ceil(arrayList.size() / 2.0);
+
+		System.out.println("kPosition: " + kPosition);
+		long startTime = System.nanoTime();
+		if (kPosition > length) {
+			System.out.println("Index out of bound");
+		} else {
+			// find kth smallest value
+			System.out.println(
+					"K-th smallest element in array : " + kthSmallest(arrayList, 0, length - 1, kPosition - 1));
+
+			System.out.println("After the partition: " + arrayList);
 		}
+		long endTime = System.nanoTime();
+		long totalTime = endTime - startTime;
+		System.out.println("Total time in nanoseconds: " + totalTime);
+		System.out.println("Counter: " + counter);
+
+	}
 }
